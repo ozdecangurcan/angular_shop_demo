665ba456bc7f624be11bb8637939b0b7d337c1fa
ozdecangurcan <gurcanozdecan@gmail.com>
2020-08-19 11:28:06 +0300

    Tasarımın İyileştirilmesi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { ProductComponent } from "./product/product.component";
 
 const routes: Routes = [
   { path: "products", component: ProductComponent },
+  { path: "product-add-cf", component: ProductComponent },
+  { path: "product-add-rf", component: ProductComponent },
   { path: "", redirectTo: "products", pathMatch: "full" },
   { path: "products/category/:categoryId", component: ProductComponent },
 ];
